08a355184e79746b78e1ca8a7a84380daa64f1ab
Carl in 't Veld <carl.intveld@cloudnation.nl>
2024-04-25 00:29:25 +0200

    kubernetes

--- nginx add-on - appgateway/kubernetes.tf
+++ nginx add-on - appgateway/kubernetes.tf
@@ -1,3 +1,18 @@
+module "kubernetes" {
+  source = "./kubernetes"
+}
+
+resource "terraform_data" "kubernetes" {
+  depends_on = [azurerm_role_assignment.aks_admin]
+  input      = azurerm_kubernetes_cluster.default
+}
+
+
+# data "azuread_service_principal" "aks" {
+#   display_name = "Azure Kubernetes Service AAD Server"
+# }
+
+
 # provider "kubernetes" {
 #   host                   = module.aks.cluster.kube_config.0.host
 #   client_certificate     = base64decode(module.aks.cluster.kube_admin_config.0.client_certificate)
@@ -5,16 +20,11 @@
 #   cluster_ca_certificate = base64decode(module.aks.cluster.kube_admin_config.0.cluster_ca_certificate)
 # }
 
-# data "azuread_service_principal" "aks" {
-#   display_name = "Azure Kubernetes Service AAD Server"
-# }
-
-provider "kubernetes" {
-  #host = module.aks.cluster.kube_config.0.host
-  host = azurerm_kubernetes_cluster.default.kube_config.0.host
+provider "kubectl" {
+  load_config_file = false
+  host             = terraform_data.kubernetes.output.kube_config.0.host
   cluster_ca_certificate = base64decode(
-    #module.aks.cluster.kube_config[0].cluster_ca_certificate,
-    azurerm_kubernetes_cluster.default.kube_config[0].cluster_ca_certificate,
+    terraform_data.kubernetes.output.kube_config[0].cluster_ca_certificate,
   )
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
@@ -29,28 +39,35 @@ provider "kubernetes" {
   }
 }
 
-resource "kubernetes_namespace" "default" {
-  metadata {
-    name = "example"
+terraform {
+  required_providers {
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
 }
 
-moved {
-  from = module.aks.azurerm_kubernetes_cluster.aks
-  to   = azurerm_kubernetes_cluster.default
+output "name" {
+  value = terraform_data.kubernetes.output.kube_config.0.host
 }
 
-moved {
-  from = module.aks.tls_private_key.tls_key["default"]
-  to   = tls_private_key.tls_key
-}
-
-moved {
-  from = module.aks.azurerm_key_vault_secret.tls_private_key_secret["default"]
-  to   = azurerm_key_vault_secret.tls_private_key_secret
-}
-
-moved {
-  from = module.aks.azurerm_key_vault_secret.tls_public_key_secret["default"]
-  to   = azurerm_key_vault_secret.tls_public_key_secret
+provider "kubernetes" {
+  #host = module.aks.cluster.kube_config.0.host
+  host = terraform_data.kubernetes.output.kube_config.0.host
+  cluster_ca_certificate = base64decode(
+    #module.aks.cluster.kube_config[0].cluster_ca_certificate,
+    terraform_data.kubernetes.output.kube_config[0].cluster_ca_certificate,
+  )
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "kubelogin"
+    args = [
+      "get-token",
+      "--login",
+      "azurecli",
+      "--server-id",
+      "6dae42f8-4368-4678-94ff-3960e28e3630" # data.azuread_service_principal.aks.client_id
+    ]
+  }
 }
